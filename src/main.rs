bfdbd7a828342bd8462c27f12e5019331a4df2ca
Edward Boland <mr.eboland@gmail.com>
2021-05-10 15:41:43 -0400

    machine types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// Machine Types
+// Rust's type system is a collection of fixed-width numeric types, chosen to match the types that almost all modern processors implement directly in hardware, and the Boolean and character types.
+
+// Size (bits) Unsigned integer Signed integer Floating-point
+// 8           u8       i8
+// 16          u16      i16
+// 32          u32      i32                    f32
+// 64          u64      i64                    f64
+// Machine word usize            isize
